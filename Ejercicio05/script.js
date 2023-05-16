ea5f8acf57186713e02c196a2e4e6d963844fb21
mhuamanivar <93347594+mhuamanivar@users.noreply.github.com>
2023-05-16 12:51:59 -0500

    Añadiendo ejercicio05

---
+++ Ejercicio05/script.js
@@ -0,0 +1,43 @@
+// Programación Web 2 - 2023 A
+// Melsy Melany Huamaní Vargas
+
+document.getElementById("formTabla").addEventListener("submit", function(event) {
+    document.getElementById("divTabla").style.display = "block";
+
+    event.preventDefault();
+    let cNumeros = parseInt(document.getElementById("cantNumeros").value);
+    crearTabla(cNumeros);
+});
+
+document.getElementById("formSuma").addEventListener('submit', function(event) {
+    document.getElementById("divSuma").style.display = "block";
+
+    event.preventDefault();
+    calcularSuma();
+});
+
+function crearTabla(cNumeros) {
+    let tabla = document.getElementById("tabla");
+    tabla.innerHTML = '';
+
+    let fila = tabla.insertRow();
+    let header = fila.insertCell();
+    header.textContent = "Numeros";
+
+    for (let i = 0; i < cNumeros; i++) {
+        let nfila = tabla.insertRow();
+        let celda = nfila.insertCell();
+        var input = document.createElement("input");
+        input.type = "number";
+        celda.appendChild(input);
+    }
+}
+
+function calcularSuma() {
+    var inputs = document.querySelectorAll('#tabla input');
+    var suma = 0;
+    for (var i = 0; i < inputs.length; i++) {
+        suma += parseInt(inputs[i].value);
+    }
+    document.getElementById("resultadoSuma").textContent = suma;
+}
